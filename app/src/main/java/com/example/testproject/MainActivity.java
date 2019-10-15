3dd753d1eb03d808fc7234086cf4a278e5064b3f
Madalina Badeanu <badeanumada@gmail.com>
2019-10-15 19:56:42 +0300

    Added simple login form

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.testproject;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.graphics.Color;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,8 +22,12 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    Button loginButton, cancelButton;
+    EditText usernameBox, passwordBox;
 
-
+    TextView tx1;
+    int counter = 3;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        loginButton = (Button)findViewById(R.id.loginButton);
+        usernameBox = (EditText)findViewById(R.id.usernameBox);
+        passwordBox = (EditText)findViewById(R.id.passwordBox);
+
+        cancelButton = (Button)findViewById(R.id.cancelButton);
+        tx1 = (TextView)findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(usernameBox.getText().toString().equals("admin") &&
+                        passwordBox.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+
+                            tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        loginButton.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
