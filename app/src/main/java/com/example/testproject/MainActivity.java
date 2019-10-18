4becd2a19f4f441004fd6c2e7c65739ce2f30148
Madalina Badeanu <badeanumada@gmail.com>
2019-10-18 14:54:51 +0300

    Cleanup

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -1,95 +1,21 @@
 package com.example.testproject;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.graphics.Color;
-
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    Button loginButton, cancelButton;
-    EditText usernameBox, passwordBox;
 
-    TextView tx1;
-    int counter = 3;
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Intent myIntent = new Intent(MainActivity.this,   AuthActivity.class);
+        MainActivity.this.startActivity(myIntent);
 
-        loginButton = (Button)findViewById(R.id.loginButton);
-        usernameBox = (EditText)findViewById(R.id.usernameBox);
-        passwordBox = (EditText)findViewById(R.id.passwordBox);
-
-        cancelButton = (Button)findViewById(R.id.cancelButton);
-        tx1 = (TextView)findViewById(R.id.textView3);
-        tx1.setVisibility(View.GONE);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(usernameBox.getText().toString().equals("admin") &&
-                        passwordBox.getText().toString().equals("admin")) {
-                    Toast.makeText(getApplicationContext(),
-                            "Redirecting...",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
-
-                            tx1.setVisibility(View.VISIBLE);
-                    tx1.setBackgroundColor(Color.RED);
-                    counter--;
-                    tx1.setText(Integer.toString(counter));
-
-                    if (counter == 0) {
-                        loginButton.setEnabled(false);
-                    }
-                }
-            }
-        });
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View view) {
-                // Write a message to the database
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("message");
-
-                myRef.setValue("Hello, World!" + (Math.random() * 1000)).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
-                        Snackbar.make(view, "Error", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-
-                    }
-                });
-            }
-        });
 
     }
 
