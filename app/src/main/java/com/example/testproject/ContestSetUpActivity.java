afdb3823cc5a1f58d669fd870fa2d7e3f0deafc1
teodoralabusca <labuscateodora@gmail.com>
2019-11-02 00:12:30 +0200

    Big fix on menu

--- app/src/main/java/com/example/testproject/ContestSetUpActivity.java
+++ app/src/main/java/com/example/testproject/ContestSetUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.testproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,12 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ContestSetUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -23,8 +26,10 @@ public class ContestSetUpActivity extends AppCompatActivity implements AdapterVi
     private static final int TOTALPOINTS = 60;
     private int pointsUsed = 0;
 
-    private EditText contestantNameBox;
-    private Button contestantSetButton;
+    private EditText setsNumberBox;
+    private Button setsButton;
+
+    private TextView rounds;
 
     private Spinner contestType;
 
@@ -43,6 +48,16 @@ public class ContestSetUpActivity extends AppCompatActivity implements AdapterVi
     private EditText interpretationNumberBox;
     private Button interpretationSetButton;
 
+    private Button submitButton;
+
+    private int originality = 0;
+    private int craftmanship = 0;
+    private int composition = 0;
+    private int unity = 0;
+    private int space = 0;
+    private int interpretation = 0;
+    private int sets = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,105 +65,20 @@ public class ContestSetUpActivity extends AppCompatActivity implements AdapterVi
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        contestantNameBox = findViewById(R.id.addcontestant);
-        contestantSetButton = findViewById(R.id.addcontestantbutton);
+        InitUIElem();
 
-        contestantSetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        criteriaSet();
 
-                String toParse = contestantNameBox.getText().toString();
-                if (TextUtils.isEmpty(toParse))
-                    contestantNameBox.setError("Required");
-                else {
-                    // add contestant to DB
-                }
-            }
-        });
+        setsSet();
 
-        contestType = findViewById(R.id.contesttype);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.contest_type, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        contestType.setAdapter(adapter);
-        contestType.setOnItemSelectedListener(this);
+        rounds.setText("Number of rounds: " +
+                Integer.toString((int) Math.ceil
+                (Math.log(getContestantsNumberFromDB())/
+                Math.log(2))));
 
-        originalityNumberBox = findViewById(R.id.originality);
-        originalitySetButton = findViewById(R.id.originalitybutton);
+        reset();
+        submit();
 
-        originalitySetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                verifyPoints(originalityNumberBox, "originality");
-
-            }
-        });
-
-        craftmanshipNumberBox = findViewById(R.id.craftmanship);
-        craftmanshipSetButton = findViewById(R.id.craftmanshipbutton);
-
-        craftmanshipSetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                verifyPoints(craftmanshipNumberBox, "craftmanship");
-            }
-        });
-
-        compositionNumberBox = findViewById(R.id.composition);
-        compositionSetButton = findViewById(R.id.compositionbutton);
-
-        compositionSetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                verifyPoints(compositionNumberBox, "composition");
-            }
-        });
-
-        unityNumberBox = findViewById(R.id.unity);
-        unitySetButton = findViewById(R.id.unitybutton);
-
-        unitySetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                verifyPoints(unityNumberBox, "unity");
-            }
-        });
-
-        spaceNumberBox = findViewById(R.id.space);
-        spaceSetButton = findViewById(R.id.spacebutton);
-
-        spaceSetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                verifyPoints(spaceNumberBox, "space");
-            }
-        });
-
-        interpretationNumberBox = findViewById(R.id.interpretation);
-        interpretationSetButton = findViewById(R.id.interpretationbutton);
-
-        interpretationSetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                verifyPoints(interpretationNumberBox, "interpretation");
-            }
-        });
-
-        reset = findViewById(R.id.pointsbutton);
-
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                pointsUsed = 0;
-            }
-        });
 
     }
 
@@ -163,21 +93,182 @@ public class ContestSetUpActivity extends AppCompatActivity implements AdapterVi
 
     }
 
-    private void verifyPoints (EditText box, String type) {
+    private void InitUIElem(){
+        setsNumberBox = findViewById(R.id.addsets);
+        setsButton = findViewById(R.id.addsetsbutton);
+
+        contestType = findViewById(R.id.contesttype);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.contest_type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        contestType.setAdapter(adapter);
+        contestType.setOnItemSelectedListener(this);
+
+        originalityNumberBox = findViewById(R.id.originality);
+        originalitySetButton = findViewById(R.id.originalitybutton);
+
+        craftmanshipNumberBox = findViewById(R.id.craftmanship);
+        craftmanshipSetButton = findViewById(R.id.craftmanshipbutton);
+
+        compositionNumberBox = findViewById(R.id.composition);
+        compositionSetButton = findViewById(R.id.compositionbutton);
+
+        unityNumberBox = findViewById(R.id.unity);
+        unitySetButton = findViewById(R.id.unitybutton);
+
+        spaceNumberBox = findViewById(R.id.space);
+        spaceSetButton = findViewById(R.id.spacebutton);
+
+        interpretationNumberBox = findViewById(R.id.interpretation);
+        interpretationSetButton = findViewById(R.id.interpretationbutton);
+
+        reset = findViewById(R.id.pointsbutton);
+
+        rounds = findViewById(R.id.rounds);
+
+        submitButton = findViewById(R.id.submit);
+
+    }
+
+    private void criteriaSet(){
+        originalitySetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                originality = verifyPoints(originalityNumberBox, originality);
+                addCriteriaToDB("originality", originality);
+
+            }
+        });
+
+        craftmanshipSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                craftmanship = verifyPoints(craftmanshipNumberBox, craftmanship);
+                addCriteriaToDB("craftmanship", craftmanship);
+            }
+        });
+
+        compositionSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                composition = verifyPoints(compositionNumberBox, composition);
+                addCriteriaToDB("composition", composition);
+            }
+        });
+
+        unitySetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                unity = verifyPoints(unityNumberBox, unity);
+                addCriteriaToDB("unity", unity);
+            }
+        });
+
+        spaceSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                space = verifyPoints(spaceNumberBox, space);
+                addCriteriaToDB("space", space);
+            }
+        });
+
+        interpretationSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                interpretation = verifyPoints(interpretationNumberBox, interpretation);
+                addCriteriaToDB("interpretation", interpretation);
+            }
+        });
+    }
+
+    private void setsSet(){
+        setsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String toParse = setsNumberBox.getText().toString();
+                if (TextUtils.isEmpty(toParse))
+                    setsNumberBox.setError("Required");
+                else {
+                    sets = Integer.parseInt(toParse);
+                    addSetsToDB(sets);
+                }
+            }
+        });
+    }
+
+    private void reset(){
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pointsUsed = 0;
+            }
+        });
+    }
+
+    private void submit() {
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sets == 0 || pointsUsed == 0) {
+                    Toast.makeText(getApplicationContext(), "Set up contest first!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent myIntent = new Intent(ContestSetUpActivity.this, AdminMenuActivity.class);
+                    ContestSetUpActivity.this.startActivity(myIntent);
+                }
+            }
+        });
+    }
+
+    private void addCriteriaToDB(String type, int number){
+        // add to DB
+    }
+
+    private int verifyPoints (EditText box, int criteria) {
         String toParse = box.getText().toString();
         if (TextUtils.isEmpty(toParse))
             box.setError("Required");
         else {
             int number = Integer.parseInt(toParse);
+            if (criteria > 0) {
+                pointsUsed -= criteria;
+            }
             if (number + pointsUsed > TOTALPOINTS) {
+                pointsUsed += criteria;
                 box.setError("Limit Exceeded");
             } else {
-                // check if already added to DB
                 pointsUsed += number;
-                // add to DB with type type
                 Toast.makeText(getApplicationContext(), "Points left: " + Integer.toString(TOTALPOINTS - pointsUsed), Toast.LENGTH_SHORT).show();
+                return number;
             }
         }
+        return 0;
+    }
+
+    private int getContestantsNumberFromDB() {
+        // get no of contestants from DB
+        return 24;
+    }
+
+    private void addSetsToDB(int number){
+
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK ) {
+            //do your stuff
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 
 }
