c2f0d91a65b76d35bea27218d10d9730d7f8de46
Irina Covrescu <irinacovrescu1306@gmail.com>
2019-11-03 16:06:59 +0200

    Refactoring on ContestantsSetUpActivity

--- app/src/main/java/com/example/testproject/AdminMenu/ContestantsSetUpActivity.java
+++ app/src/main/java/com/example/testproject/AdminMenu/ContestantsSetUpActivity.java
@@ -25,13 +25,13 @@ public class ContestantsSetUpActivity extends AppCompatActivity {
         so they can be disqualified as needed
      */
     private static final int CONTESTANTSPERSERIES = 10;
-    private int contestantsNumber = 0;
-    private int contestantsLeft;
+    private int participantsNumber = 0;
+    private int participantsLeft;
 
-    private EditText contestantNameBox;
-    private Button contestantSetButton;
-    private EditText contestantNumberBox;
-    private Button contestantAddButton;
+    private EditText participantNameBox;
+    private Button participantSetButton;
+    private EditText participantNumberBox;
+    private Button participantAddButton;
     private Button submitButton;
 
 
@@ -41,97 +41,91 @@ public class ContestantsSetUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contestants_set_up);
 
         InitUIElem();
-        contestantsNumber();
-        contestantAdd();
+        participantsNumber();
+        participantAdd();
         submit();
 
     }
 
     private void InitUIElem() {
-        contestantNameBox = findViewById(R.id.contestantsname);
-        contestantAddButton = findViewById(R.id.contestantsadd);
+        participantNameBox = findViewById(R.id.contestantsname);
+        participantAddButton = findViewById(R.id.contestantsadd);
 
-        contestantNumberBox = findViewById(R.id.contestantsnumber);
-        contestantSetButton = findViewById(R.id.contestantsset);
+        participantNumberBox = findViewById(R.id.contestantsnumber);
+        participantSetButton = findViewById(R.id.contestantsset);
 
         submitButton = findViewById(R.id.submit);
     }
 
-    private void contestantsNumber() {
-        contestantSetButton.setOnClickListener(new View.OnClickListener() {
+    private void participantsNumber() {
+        participantSetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String toParse = contestantNumberBox.getText().toString();
+                String toParse = participantNumberBox.getText().toString();
                 if (TextUtils.isEmpty(toParse)) {
-                    contestantNumberBox.setError("Required");
+                    participantNumberBox.setError("Required");
                 } else {
-                    contestantsNumber = Integer.parseInt(toParse);
-                    if (contestantsNumber == 0) {
-                        contestantNumberBox.setError("Can't be 0");
+                    participantsNumber = Integer.parseInt(toParse);
+                    if (participantsNumber == 0) {
+                        participantNameBox.setError("Can't be 0");
                     } else {
-                        contestantsLeft = contestantsNumber;
-                        addNumberOfContestantsToDB(contestantsNumber);
-                        Toast.makeText(getApplicationContext(), "Contestants number set to " + Integer.toString(contestantsNumber), Toast.LENGTH_SHORT).show();
+                        participantsLeft = participantsNumber;
+                        addNumberOfParticipantsToDB(participantsNumber);
+                        Toast.makeText(getApplicationContext(), "Participants number set to " + participantsNumber, Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
     }
 
-    private void contestantAdd(){
-        contestantAddButton.setOnClickListener(new View.OnClickListener() {
+    private void participantAdd(){
+        participantAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String toParse = contestantNameBox.getText().toString();
+                String toParse = participantNameBox.getText().toString();
                 if (TextUtils.isEmpty(toParse)) {
-                    contestantNameBox.setError("Required");
+                    participantNameBox.setError("Required");
                 } else {
-                    if (contestantsLeft > 0) {
-                        int id = contestantsNumber - contestantsLeft + 1;
-                        addParticipant(Integer.toString(setSeries(id)), Integer.toString(id), toParse, setRounds());
-                        contestantsLeft--;
-                        contestantNameBox.getText().clear();
-                        Toast.makeText(getApplicationContext(), "Contestants left to add: " + Integer.toString(contestantsLeft), Toast.LENGTH_SHORT).show();
+                    if (participantsLeft > 0) {
+                        int id = participantsNumber - participantsLeft + 1;
+                        final DatabaseReference databaseRef =  FirebaseDatabase.getInstance().getReference("participants").child(Integer.toString(setSeries(id)));
+
+                        Participant p = new Participant(toParse,setRounds());
+                        databaseRef.child(Integer.toString(id)).setValue(p);
+                        participantsLeft--;
+                        participantNameBox.getText().clear();
+                        Toast.makeText(getApplicationContext(), "Participants left to add: " + participantsLeft, Toast.LENGTH_SHORT).show();
                     } else {
-                        Toast.makeText(getApplicationContext(), "Too many contestants!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Too many participants!", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
     }
 
-    public void addParticipant(String series, String id, String name, Integer rounds){
 
-        //Obtine referinta catre baza de date la adresa dorita
-        final DatabaseReference databaseRef =  FirebaseDatabase.getInstance().getReference("participants").child(series);
-
-        Participant p = new Participant(name,rounds);
-        databaseRef.child(id).setValue(p);
-
-    }
-
-    private void addNumberOfContestantsToDB(int number) {
+    private void addNumberOfParticipantsToDB(int number) {
         // add to DB
     }
 
     private int setRounds(){
-        return (int) Math.ceil(Math.log(contestantsNumber)/ Math.log(2));
+        return (int) Math.ceil(Math.log(participantsNumber)/ Math.log(2));
     }
 
     private int setSeries(int id){
 
-        int series = contestantsNumber / CONTESTANTSPERSERIES;
+        int series = participantsNumber / CONTESTANTSPERSERIES;
         int new_cps;
         if (series == 0) {
             new_cps = CONTESTANTSPERSERIES;
         } else {
-            new_cps = CONTESTANTSPERSERIES + ((contestantsNumber % CONTESTANTSPERSERIES) / series);
+            new_cps = CONTESTANTSPERSERIES + ((participantsNumber % CONTESTANTSPERSERIES) / series);
         }
         ArrayList<Integer> end = new ArrayList<Integer>(series);
         int offset = 0;
         for (int i = 0; i < series; i++)
         {
-            end.add(i, (i + 1) * new_cps + Math.min(contestantsNumber%new_cps, ++offset));
+            end.add(i, (i + 1) * new_cps + Math.min(participantsNumber%new_cps, ++offset));
         }
         int x_series = 1;
         for (int i = 0; i < series; i++)
@@ -149,8 +143,8 @@ public class ContestantsSetUpActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if((contestantsNumber == 0) || (contestantsLeft != 0)) {
-                    Toast.makeText(getApplicationContext(), "Set up contestants first!", Toast.LENGTH_SHORT).show();
+                if((participantsNumber == 0) || (participantsLeft != 0)) {
+                    Toast.makeText(getApplicationContext(), "Set up participants first!", Toast.LENGTH_SHORT).show();
                 } else {
                     Intent myIntent = new Intent(ContestantsSetUpActivity.this, AdminMenuActivity.class);
                     ContestantsSetUpActivity.this.startActivity(myIntent);
