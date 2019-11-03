773a9e49374345ff15759fdf0646d1706fcf4640
Madalina Badeanu <badeanumada@gmail.com>
2019-11-03 14:29:44 +0200

    Got criteria from database

---
+++ app/src/main/java/com/example/testproject/Data/MyCallback.java
@@ -0,0 +1,9 @@
+package com.example.testproject.Data;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+public interface MyCallback {
+
+    void onCallBack(ArrayList<Criteria> value);
+}
