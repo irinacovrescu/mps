a746f7884d8b42285c511108101b0ea4f627f09a
teodoralabusca <labuscateodora@gmail.com>
2019-11-04 02:10:32 +0200

    Small update: table for leaderboard

--- app/src/main/java/com/example/testproject/LeaderBoardActivity.java
+++ app/src/main/java/com/example/testproject/LeaderBoardActivity.java
@@ -15,12 +15,18 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 public class LeaderBoardActivity extends AppCompatActivity {
     int nrOfRounds;
-    LinearLayout roundsLayout;
+    LinearLayout roundsLayout, tableLayout;
+    ViewFlipper vf;
+    Button back;
+    TableLayout tl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,9 @@ public class LeaderBoardActivity extends AppCompatActivity {
                 (Math.log(ContestSetUpActivity.getParticipantsNumberFromDB())/
                         Math.log(2));
         roundsLayout = findViewById(R.id.roundsExtended);
+        tableLayout = findViewById(R.id.linearlayout);
+        vf = findViewById(R.id.viewflipper);
+        back = findViewById(R.id.back);
         for (int i = 1; i <= nrOfRounds; i++) {
             createRoundButtons(i, "Round " + i);
         }
@@ -48,7 +57,33 @@ public class LeaderBoardActivity extends AppCompatActivity {
         participantButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //TODO:
+                vf.showNext();
+                display();
+                back.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        tl.removeAllViews();
+                        vf.showPrevious();
+                    }
+                });
             }
         });
+    }
+
+    private void display(){
+        tl = new TableLayout(this);
+
+        TableRow row = new TableRow(this);
+        TextView cellN = new TextView(this);
+        cellN.setText("Name");
+        row.addView(cellN);
+        TextView cellS = new TextView(this);
+        cellS.setText("Series");
+        row.addView(cellS);
+        TextView cellD = new TextView(this);
+        cellD.setText("Points");
+        row.addView(cellD);
+        tl.addView(row);
+        tableLayout.addView(tl);
     }
 }
